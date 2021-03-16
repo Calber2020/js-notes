98bcec8da0c7b8d8d036337d189a77015c41d615
Chase <calber@students.solano.edu>
2021-03-16 10:34:45 -0700

    refractoring

--- app.js
+++ app.js
@@ -84,15 +84,30 @@ const warriorsGames = [{
   }
 ]
 
-const ulParent = document.createElement('ul');
-for(let game of warriorsGames){
-  const {
-    homeTeam,
-    awayTeam
-  } = game;
 
-  const gameLi = document.createElement('li');
 
+const makeChart = (games) => {
+  const ulParent = document.createElement('ul');
+  for(let game of games){
+    const {
+      homeTeam,
+      awayTeam
+    } = game;
+
+    const gameLi = document.createElement('li');
+    gameLi.innerHTML = getScoreLine(game);
+
+    
+
+    // const warriors = hTeam === 'Golden State' ? homeTeam : awayTeam;
+    // gameLi.classList.add(warriors.isWinner?'win':'loss');
+
+    ulParent.appendChild(gameLi);
+  }
+  return ulParent;
+};
+
+const getScoreLine = ({homeTeam, awayTeam}) => {
   const {
     team:hTeam,
     points: hPoints
@@ -111,12 +126,9 @@ for(let game of warriorsGames){
   } else {
     scoreLine = `${aPoints}-<b>${hPoints}</b>`;
   }
-
-  const warriors = hTeam === 'Golden State' ? homeTeam : awayTeam;
-  gameLi.classList.add(warriors.isWinner?'win':'loss');
-
-  gameLi.innerHTML = `${teamNames} ${scoreLine}`;
-  ulParent.appendChild(gameLi);
+  return `${teamNames} ${scoreLine}`;
 }
 
-document.body.prepend(ulParent)
+const chart1 = makeChart(warriorsGames, 'Golden State');
+// const chart1 = makeChart(warriorsGames, 'Houston');
+document.body.prepend(chart1);
