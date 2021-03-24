5cd6602cd24697001d182ceac0c7180aea760676
Chase <calber@students.solano.edu>
2021-03-24 11:14:54 -0700

    finished assignment

--- assignment1/app.js
+++ assignment1/app.js
@@ -2,8 +2,17 @@ const app = Vue.createApp({
   data() {
     return {
       name: 'Chase',
-      age: 22
+      age: 22,
+      imageUrl: "https://wallpapercave.com/wp/wp3789020.jpg"
     };
+  },
+  methods: {
+    calculateAge() {
+      return this.age + 5;
+    },
+    calculateRandom() {
+      return Math.random();
+    }
   }
 });
 
